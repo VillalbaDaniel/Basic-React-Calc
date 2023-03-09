4d082ffe0bba308d4737a7c602db7fff04686285
Dani <jdvillalba2@hotmail.com>
2023-03-09 14:30:19 +0100

    Add Calc to the repository

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Calculadora from "./componentes/Calculadora";
+
+
+function App() {
+
+
+  return (
+      
+    <div className="App">
+    <Calculadora/>
+    </div>
+      
+  );
+}
+
+export default App;
---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,9 @@
+import "../hojas-de-estilos/Pantalla.css";
+
+const Pantalla = ({ input }) => (
+<div className="input">
+    {input}
+    </div>
+    );
+
+export default Pantalla;
